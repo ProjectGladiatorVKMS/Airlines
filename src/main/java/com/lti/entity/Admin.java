156e66b38efbb7b807b59ff3f1141b72ab0a45ae
vinay <Vinay.Bojja@lntinfotech.com>
2021-01-10 14:42:26 +0530

    admin.java -getters and setters

--- src/main/java/com/lti/entity/Admin.java
+++ src/main/java/com/lti/entity/Admin.java
@@ -20,4 +20,30 @@ public class Admin {
 	
 	@Column(name="admin_password")
 	private String adminPassword;
+
+	public int getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+
+	public String getAdminUsername() {
+		return adminUsername;
+	}
+
+	public void setAdminUsername(String adminUsername) {
+		this.adminUsername = adminUsername;
+	}
+
+	public String getAdminPassword() {
+		return adminPassword;
+	}
+
+	public void setAdminPassword(String adminPassword) {
+		this.adminPassword = adminPassword;
+	}
+	
+	
 }
