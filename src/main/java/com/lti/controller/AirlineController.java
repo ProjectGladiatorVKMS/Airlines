88fd887f1ca2253c109c42b6d75b6cb083ad3c34
vinay <Vinay.Bojja@lntinfotech.com>
2021-01-15 13:47:07 +0530

    Admin Module

--- src/main/java/com/lti/controller/AirlineController.java
+++ src/main/java/com/lti/controller/AirlineController.java
@@ -30,8 +30,8 @@ import com.lti.service.LoginService;
 @CrossOrigin
 public class AirlineController {
 
-	@Autowired
-	private AdminService adminService;
+//	@Autowired
+//	private AdminService adminService;
 
 	@Autowired
 	private AirlineService airlineService;
@@ -53,49 +53,49 @@ public class AirlineController {
 //	}
 
 	/*----------------------------------------------------------Admin Controller------------------------------------------------------------------*/
-	@PostMapping("/addFlight")
-	public String addFlight(@RequestBody FlightDataTransfer flightDT) {
-		System.out.println("Airline controller");
-		Flight flight = new Flight();
-		Admin admin = new Admin();
-		admin.setAdminId(flightDT.getAdminId());
-
-		flight.setSource(flightDT.getSource());
-		flight.setDestination(flightDT.getDestination());
-		flight.setJourneyDate(flightDT.getJourneyDate());
-		flight.setDeparture(flightDT.getDepartureTime());
-		flight.setArrival(flightDT.getArrivalTime());
-		flight.setEconomyClassCost(flightDT.getEconomyClassCost());
-		flight.setBusinessClassCost(flightDT.getBusinessClassCost());
-		flight.setNoOfSeats(flightDT.getNoOfSeats());
-		flight.setEconomySeats(flightDT.getEconomySeats());
-		flight.setBusinessSeats(flightDT.getBusinessSeats());
-		flight.setAdmin(admin);
-		System.out.println("Airline controller");
-		adminService.addFlight(flight);
-
-		return null;
-	}
-
-	@DeleteMapping("/deleteFlight")
-	public String deleteFlight(@RequestParam("id") int FlightId) {
-		adminService.deleteFight(FlightId);
-		return null;
-	}
-
-	@GetMapping("/displayAllFlights")
-	public List<Flight> displayAllFlights() {
-		List<Flight> flightList = adminService.displayAllFlights();
-		return flightList;
-	}
-
-	@GetMapping("/displayFlight")
-	public Flight displayFlight(@RequestParam("id") int flightId) {
-		System.out.println(flightId);
-		Flight flight = adminService.displayFlight(flightId);
-		System.out.println(flight);
-		return flight;
-	}
+//	@PostMapping("/addFlight")
+//	public String addFlight(@RequestBody FlightDataTransfer flightDT) {
+//		System.out.println("Airline controller");
+//		Flight flight = new Flight();
+//		Admin admin = new Admin();
+//		admin.setAdminId(flightDT.getAdminId());
+//
+//		flight.setSource(flightDT.getSource());
+//		flight.setDestination(flightDT.getDestination());
+//		flight.setJourneyDate(flightDT.getJourneyDate());
+//		flight.setDeparture(flightDT.getDepartureTime());
+//		flight.setArrival(flightDT.getArrivalTime());
+//		flight.setEconomyClassCost(flightDT.getEconomyClassCost());
+//		flight.setBusinessClassCost(flightDT.getBusinessClassCost());
+//		flight.setNoOfSeats(flightDT.getNoOfSeats());
+//		flight.setEconomySeats(flightDT.getEconomySeats());
+//		flight.setBusinessSeats(flightDT.getBusinessSeats());
+//		flight.setAdmin(admin);
+//		System.out.println("Airline controller");
+//		adminService.addFlight(flight);
+//
+//		return null;
+//	}
+//
+//	@DeleteMapping("/deleteFlight")
+//	public String deleteFlight(@RequestParam("id") int FlightId) {
+//		adminService.deleteFight(FlightId);
+//		return null;
+//	}
+//
+//	@GetMapping("/displayAllFlights")
+//	public List<Flight> displayAllFlights() {
+//		List<Flight> flightList = adminService.displayAllFlights();
+//		return flightList;
+//	}
+//
+//	@GetMapping("/displayFlight")
+//	public Flight displayFlight(@RequestParam("id") int flightId) {
+//		System.out.println(flightId);
+//		Flight flight = adminService.displayFlight(flightId);
+//		System.out.println(flight);
+//		return flight;
+//	}
 
 	/*
 	 * @GetMapping("/hello") public String hello(@RequestParam("name") String name)
